61000eb0b76eea39e583eb11dde802b485529012
cp-23fam <fabian.marti@ceff.ch>
2025-04-11 15:19:12 +0200

    ➕ Toekn on login

--- Cefform/Controllers/GeneralController.cs
+++ Cefform/Controllers/GeneralController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.DirectoryServices.AccountManagement;
 using System.Security.Cryptography;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace Cefform.Controllers
@@ -12,17 +14,27 @@ namespace Cefform.Controllers
     public class GeneralController : ControllerBase
     {
         
-        [HttpGet("login")]
+        [HttpPost("login")]
         public IActionResult GetLogin(string user, string pwd)
         {
             string clear = Encryption.Instance.Decrypt(pwd);
 
             using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "INTRA.CEFF.CH"))
-                if (pc.ValidateCredentials(user, pwd)) {
-                    return Ok();
+                if (pc.ValidateCredentials(user, clear)) {
+                    using (SHA512 sha512 = SHA512.Create())
+                    {
+                        byte[] inputBytes = Encoding.UTF8.GetBytes(user + DateTime.Now.ToString());
+                        byte[] hashBytes = sha512.ComputeHash(inputBytes);
+                        StringBuilder sb = new StringBuilder();
+                        for (int i = 0; i < hashBytes.Length; i++)
+                        {
+                            sb.Append(hashBytes[i].ToString("X2"));
+                        }
+                        return Ok(sb.ToString());
+                    }
                 }
 
-            return BadRequest();
+            return Unauthorized();
 
         }
 
